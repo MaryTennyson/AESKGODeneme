25517cdb283800e554002776fc19cd09b396547e
MaryTennyson <ebrar-atabay@hotmail.com>
2023-07-07 23:38:06 +0300

    Current Locations Permissions added

--- app/src/main/java/com/ebraratabay/aeskgodeneme/MapsActivity.kt
+++ app/src/main/java/com/ebraratabay/aeskgodeneme/MapsActivity.kt
@@ -1,5 +1,6 @@
 package com.ebraratabay.aeskgodeneme
 
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -20,6 +21,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.ebraratabay.aeskgodeneme.databinding.ActivityMapsBinding
 import com.google.android.material.snackbar.Snackbar
+import java.util.prefs.AbstractPreferences
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -28,13 +30,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
-
+    private  lateinit var sharedPreferences: SharedPreferences
+    private var trackBoolean: Boolean? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+       sharedPreferences= this.getSharedPreferences("com.ebraratabay.aeskgodeneme", MODE_PRIVATE)
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
     registerLauncher()
+        trackBoolean=false
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -56,9 +60,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         locationListener= object: LocationListener {
             override fun onLocationChanged(location: Location) {
+                trackBoolean=sharedPreferences.getBoolean("trackBoolean",false)
+                if(!trackBoolean!!){
+                    val userLocation= LatLng(location.latitude , location.longitude)
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15f))
+                    sharedPreferences.edit().putBoolean("trackBoolean",true).apply()
+                }
                 println("location:"+location.toString())
-
-            }
+               }
 
 
         }
@@ -73,7 +82,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }else{
            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,10f,locationListener)
+           val lastLocation= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if(lastLocation!=null){
+               val lastUserLocation= LatLng(lastLocation.latitude,lastLocation.longitude)
+               mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lastUserLocation,15f))
 
+            }
+            mMap.isMyLocationEnabled=true
         }
 
 
@@ -85,15 +100,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 */
     }
     private fun registerLauncher(){
- permissionLauncher= registerForActivityResult(ActivityResultContracts.RequestPermission()){
-     result->
-     if(result){
+     permissionLauncher= registerForActivityResult(ActivityResultContracts.RequestPermission()){
+     result-> if(result){
          if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
          locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,10f,locationListener)
 
      }else{
          Toast.makeText(this@MapsActivity,"Permission needed!",Toast.LENGTH_LONG).show()
      }
+
  }
     }
 }
